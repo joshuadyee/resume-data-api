d8dd7474ec63e581401a4387eb157604c30c9917
Joshua Yee <joshyee16@gmail.com>
2024-01-30 10:54:27 -0800

    add sessions create action (login)

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2024_01_30_032335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "skills", force: :cascade do |t|
-    t.string "skill_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "student_skills", force: :cascade do |t|
-    t.integer "student_id"
-    t.integer "skill_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "capstones", force: :cascade do |t|
     t.integer "student_id"
@@ -34,23 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_032335) do
     t.string "description"
     t.string "url"
     t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.string "bio"
-    t.string "linkedin_url"
-    t.string "twitter_handle"
-    t.string "website_url"
-    t.string "resume_url"
-    t.string "github_url"
-    t.string "photo"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -73,6 +42,36 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_032335) do
     t.string "job_title"
     t.string "company"
     t.string "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_skills", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "bio"
+    t.string "linkedin_url"
+    t.string "twitter_handle"
+    t.string "website_url"
+    t.string "resume_url"
+    t.string "github_url"
+    t.string "photo"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
